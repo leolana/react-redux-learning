1378abb47e501687383f7a400232f04a6f96e4a8
Leonardo Lana <leonardo@goomer.com.br>
2018-08-21 04:13:46 -0300

    Add project

---
+++ demo-todolist/src/App.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+import TodoHeader from './TodoHeader';
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
+
+import logo from "./logo.svg";
+import "./App.css";
+
+const todoItems = [
+  {index: 1, value: "learn react", done: false},
+  {index: 2, value: "Go shopping", done: true},
+  {index: 3, value: "buy flowers", done: true}
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.addItem = this.addItem.bind(this);
+    this.removeItem = this.removeItem.bind(this);
+    this.markTodoDone = this.markTodoDone.bind(this);
+    this.state = { todoItems: todoItems };
+  }
+  addItem(todoItem) {
+    todoItems.unshift({
+      index: todoItems.length + 1,
+      value: todoItem.newItemValue,
+      done: false
+    });
+    this.setState({ todoItems: todoItems });
+  }
+  removeItem(itemIndex) {
+    todoItems.splice(itemIndex, 1);
+    this.setState({ todoItems: todoItems });
+  }
+  markTodoDone(itemIndex) {
+    var todo = todoItems[itemIndex];
+    todoItems.splice(itemIndex, 1);
+    todo.done = !todo.done;
+    todo.done ? todoItems.push(todo) : todoItems.unshift(todo);
+    this.setState({ todoItems: todoItems });
+  }
+  render() {
+    return (
+      <div id="main">
+        <TodoHeader />
+        <TodoList
+          items={this.props.initItems}
+          removeItem={this.removeItem}
+          markTodoDone={this.markTodoDone}
+        />
+        <TodoForm addItem={this.addItem} />
+      </div>
+    );
+  }
+}
+
+export default App;
